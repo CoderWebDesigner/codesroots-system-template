82563ea44c4755418bc820b82796acca59a195ab
Ahmed Ramzy <ahmedramzy_fcih@yahoo.com>
2018-10-19 18:43:03 +0200

    edit

---
+++ js/stages.js
@@ -0,0 +1,11 @@
+// alert($('.stages p.completed').length)
+
+// function sendBrowserNotification(){
+//     for(var i= 0;i<$('.stages p.completed').length;i++){
+//         var myNotification = new Notify('Yo dawg!', {
+//             body: 'This is an awesome notification',
+//             notifyShow: onNotifyShow
+//         });
+//     }
+// }
+// sendBrowserNotification()
